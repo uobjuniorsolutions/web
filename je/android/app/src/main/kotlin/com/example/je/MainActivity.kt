808b5c12a9b984fddef98a5af6e5dc3ddd09ec0c
HHazard2002 <hugo7.hazard@gmail.com>
2023-02-06 18:52:06 +0000

    ok

--- je/android/app/src/main/kotlin/com/example/je/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.je
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- je/lib/data/email.dart
+++
@@ -1,8 +0,0 @@
-class sendEmail{
-  String name;
-  String email;
-  String subject;
-  String message;
-  sendEmail(this.email,this.message,this.subject,this.name);
-
-}
